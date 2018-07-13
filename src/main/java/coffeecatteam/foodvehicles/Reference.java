56bcf95cdfa62c1d1e1c2a62c9d4ccac6e975828
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-07-13 17:19:16 +1000

    Updated Vehicle Mod to version 0.17.+

--- src/main/java/coffeecatteam/foodvehicles/Reference.java
+++ src/main/java/coffeecatteam/foodvehicles/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 	public static final String NAME = "Food Vehicles";
 	public static final String VERSION = "1.2";
 	
-	public static final String DEPENDCIES = "required-after:vehicle@[0.11.0,);";
+	public static final String DEPENDCIES = "required-after:vehicle@[0.17.0,);";
 
 	public static final String CLIENTPROXY = "coffeecatteam.foodvehicles.proxy.ProxyClient";
 	public static final String COMMONPROXY = "coffeecatteam.foodvehicles.proxy.ProxyCommon";
